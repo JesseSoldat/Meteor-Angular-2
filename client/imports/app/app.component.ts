1813ae78cf62aa3230801a27b0c3da408aef862b
jesse <jsoldat@hotmail.com>
2017-01-25 15:25:49 +0700

    can insert documents into the collection through a form

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Parties } from '../../../both/collections/parties.collection';
+import { Party } from '../../../both/models/party.model';
 
 import template from './app.component.html';
  
@@ -6,4 +9,25 @@ import template from './app.component.html';
   selector: 'app',
   template
 })
-export class AppComponent {}
+export class AppComponent {
+
+	parties: Observable<Party[]>;
+
+	constructor(){
+		 this.parties = Parties.find({}).zone();
+		// this.parties = [
+  //     {'name': 'Dubstep-Free Zone',
+  //       'description': 'Can we please just for an evening not listen to dubstep.',
+  //       'location': 'Palo Alto'
+  //     },
+  //     {'name': 'All dubstep all the time',
+  //       'description': 'Get it on!',
+  //       'location': 'Palo Alto'
+  //     },
+  //     {'name': 'Savage lounging',
+  //       'description': 'Leisure suit required. And only fiercest manners.',
+  //       'location': 'San Francisco'
+  //     }
+  //   ];
+	}
+}
---
+++ client/imports/app/parties/parties-form.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { Parties } from '../../../../both/collections/parties.collection';
+
+import template from './parties-form.component.html';
+ 
+@Component({
+  selector: 'parties-form',
+  template
+})
+export class PartiesFormComponent implements OnInit {
+	addForm: FormGroup;
+
+	constructor(private formBuilder: FormBuilder){
+
+	}
+
+	ngOnInit(){
+		this.addForm = this.formBuilder.group({
+			name: ['Jesse', Validators.required],
+			description: [],
+			location: ['', Validators.required]
+		});
+		// console.log(this.addForm.value);
+		// console.log(this.addForm.valid)
+	}
+
+	addParty():void{
+		if(this.addForm.valid){
+			Parties.insert(this.addForm.value);
+
+			this.addForm.reset();
+		}
+	}
+}
